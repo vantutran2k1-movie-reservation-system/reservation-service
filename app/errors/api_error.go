6c678d617ef62f69aff74c897ed081950f6a37d5
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-09-02 13:14:35 +0700

    Create custom api error

---
+++ app/errors/api_error.go
@@ -0,0 +1,30 @@
+package errors
+
+import (
+	"fmt"
+	"net/http"
+)
+
+type ApiError struct {
+	StatusCode int
+	Message    string
+}
+
+func (e *ApiError) Error() string {
+	return e.Message
+}
+
+func BadRequestError(format string, args ...any) *ApiError {
+	return newError(http.StatusBadRequest, fmt.Sprintf(format, args...))
+}
+
+func InternalServerError(format string, args ...any) *ApiError {
+	return newError(http.StatusInternalServerError, fmt.Sprintf(format, args...))
+}
+
+func newError(statusCode int, message string) *ApiError {
+	return &ApiError{
+		StatusCode: statusCode,
+		Message:    message,
+	}
+}
