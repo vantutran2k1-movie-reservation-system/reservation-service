8e44dbe9cb5f996c1d905714c72715cb38df4f37
vantutran2k1 <vantu.tran2k1@gmail.com>
2024-10-06 21:10:31 +0700

    Update mock file names and gen mock script

--- scripts/gen_mock.sh
+++ scripts/gen_mock.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
-if [ "$#" -ne 2 ]; then
-    echo "Usage: $0 <source> <destination>"
+# Check if the number of arguments is less than 1
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <source>"
     exit 1
 fi
 
+# Assign the source argument to a variable
 SOURCE=$1
-DESTINATION=$2
 
-PACKAGE_NAME=$(basename "$(dirname "$DESTINATION")")
+# Extract folder name and file name from source path
+FOLDER_NAME=$(basename "$(dirname "$SOURCE")")
+FILE_NAME=$(basename "$SOURCE")
 
-mockgen -source="$SOURCE" -destination="$DESTINATION" -package="$PACKAGE_NAME"
+# Construct the destination path
+MOCK_DESTINATION="app/mocks/mock_${FOLDER_NAME}/${FILE_NAME}"
+
+# Create the destination directory if it doesn't exist
+mkdir -p "$(dirname "$MOCK_DESTINATION")"
+
+# Extract package name from the destination folder
+PACKAGE_NAME=$(basename "$(dirname "$MOCK_DESTINATION")")
+
+# Run mockgen command with dynamic destination and package name
+mockgen -source="$SOURCE" -destination="$MOCK_DESTINATION" -package="$PACKAGE_NAME"
+
+echo "Mock generated at $MOCK_DESTINATION"
